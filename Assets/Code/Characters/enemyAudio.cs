583a3878f0a0d1b027dcb5bcc1bcd203772f191f
kyle <kylengelmann@gmail.com>
2018-09-02 14:10:57 -0500

    Sounds when enemy charging up

--- Assets/Code/Characters/enemyAudio.cs
+++ Assets/Code/Characters/enemyAudio.cs
@@ -6,12 +6,19 @@ public class enemyAudio : MonoBehaviour {
 
   public AudioSource enemySounds;
   public AudioClip enemySwoosh;
+    public AudioClip enemyCharge;
 
   public void woosh()
   {
     enemySounds.clip = enemySwoosh;
     enemySounds.Play();
   }
+   
+    public void charge()
+    {
+        enemySounds.clip = enemyCharge;
+        enemySounds.Play();
+    }
 
 	// Use this for initialization
 	void Start () {
