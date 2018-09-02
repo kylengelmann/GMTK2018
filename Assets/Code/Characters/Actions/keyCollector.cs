7a3735acccbef47f00e7f84322db765cadecadb5
kyle <kylengelmann@gmail.com>
2018-09-01 23:43:56 -0500

    Keys

---
+++ Assets/Code/Characters/Actions/keyCollector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyCollector : MonoBehaviour {
+
+    public int numKeys = 0;
+	public KeyDisplay display;
+
+    public void addKey()
+    {
+        numKeys++;
+        display.addKey();
+    }
+
+    private void reset()
+    {
+        numKeys = 0;
+        display.reset();
+    }
+
+}
---
+++ Assets/Code/Environment/KeyDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDisplay : MonoBehaviour {
+
+	public float padding = 50f;
+
+    public GameObject keyTemplate;
+
+    List<GameObject> keys = new List<GameObject>();
+
+    public void addKey()
+    {
+        GameObject newKey = Instantiate(keyTemplate, transform);
+        newKey.GetComponent<RectTransform>().anchoredPosition = new Vector2(keys.Count*padding, 0f);
+        keys.Add(newKey);
+    }
+
+    public void reset()
+    {
+        foreach(GameObject key in keys)
+        {
+            Destroy(key);
+        }
+        keys.RemoveAll(key => { return true;});
+    }
+}
