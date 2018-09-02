6c595c443565329440157d0b5ad7e59d64cc6fd7
goastie <nekoaiyce@gmail.com>
2018-09-02 02:04:28 -0500

    sounds
    added some sounds

---
+++ Assets/Code/Characters/playerAudio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAudio : MonoBehaviour {
+
+  public AudioSource playerSounds;
+  public AudioClip ouch1;
+  public AudioClip ouch2;
+  public AudioClip ouch3;
+  public AudioClip ouch4;
+  public AudioClip ouch5;
+  public AudioClip dieSound;
+  public AudioClip moveSound;
+  public AudioClip keyPickUp;
+
+  public void ouch()
+  {
+    playerSounds.clip = ouch1;
+    playerSounds.Play();
+  }
+
+  public void die()
+  {
+    playerSounds.clip = dieSound;
+    playerSounds.Play();
+  }
+
+  public void move()
+  {
+    playerSounds.clip = moveSound;
+    playerSounds.Play();
+  }
+
+  
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Code/chestAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chestAudio : MonoBehaviour {
+
+  public AudioSource chestSound;
+  public AudioClip chestOpen;
+
+  public void opening()
+  {
+    chestSound.clip = chestOpen;
+    chestSound.Play();
+  }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
